709871d1e8a52649913369f68d76d3c6717b9fdd
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-18 23:52:53 +0300

    feat: add enums

---
+++ Assets/Scripts/Enums/UnitType.cs
@@ -0,0 +1,16 @@
+namespace RTSGame.Enums
+{
+    public enum UnitType
+    {
+        Paladin,
+        DemonHunter,
+        Warrior,
+        Rogue,
+        Shaman,
+        Priest,
+        Hunter,
+        Mage,
+        Warlock,
+        Druid
+    }
+}
