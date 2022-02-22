1495a43be1c951025a7d49821f4d230c5e13a6b1
Gencer Güzeloğlu <gencer.guzeloglu@outlook.com>
2022-02-22 20:49:03 +0300

    feat: add new model to keep track of progression of heroes

---
+++ Assets/Scripts/Concretes/Models/ProgressModel.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace RTSGame.Concretes.Models
+{
+    public class ProgressModel
+    {
+        public string Name;
+        public int GainedExperience;
+        public int GainedLevel;
+        public float GainedHealth;
+        public float GainedAttackPower;
+        public Color UnitColor;
+    }
+}
